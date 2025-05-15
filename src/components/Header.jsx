347fa794559e7fb8e1d7046e20f107b60f122998
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-05-16 04:05:53 +0800

    Changed the h1 again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,12 +15,12 @@ const Header = () => {
 
   <div className="flex flex-col w-full ">
     <div className="flex justify-between items-center pt-0.5">
-      <h1 className="text-2xl md:text-4xl font-extrabold leading-tight">Gabriel Eduria</h1>
+      <h1 className="text-2xl md:text-4xl font-extrabold whitespace-nowrap overflow-hidden">Gabriel Eduria</h1>
         <DarkMode />
     </div>
 
     <p className="text-xs md:text-sm text-gray-400">📍 Manila, Philippines</p>      
-    <p className="pt-1 py-1 text-sm md:text-base">
+    <p className="pt-1 py-1 leading-tight overflow-hidden text-sm md:text-base">
       <span className="text-blue-500 font-bold">Software Developer </span> |
       <span className="text-red-500 font-bold"> Fitness Coach </span>
     </p>
