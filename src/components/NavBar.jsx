b9934720c1c7fede5f1befde8d1fe0831c1f06d9
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-05-26 16:48:40 +0800

    fixed navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
           mx-auto max-w-[880px] flex items-center justify-between px-4 h-full w-full bg-brown-900 rounded-full bg-clip-padding 
           backdrop-filter backdrop-blur-sm bg-opacity-50 py-3
         
-        text-gray-900 dark:text-gray-300 ">
+        text-gray-900 dark:text-white">
           <Link to="/Home" className="inline-flex items-center text-xs md:text-sm transition transform duration-200 hover:scale-105
                                       dark:text-gray-300">
             <i className="fa-solid fa-arrow-left mr-2" /> Home
