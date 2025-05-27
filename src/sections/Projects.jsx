ba8ef18a9409e6340f7a9f3db42f25344da2e5d6
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-05-28 03:39:53 +0800

    -v

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -13,7 +13,7 @@ const Projects = () => {
   const { isDarkMode } = useContext(DarkModeContext);
 
   return (
-     <div className="relative w-full min-h-screen">
+     <div className="relative w-full min-h-screen overflow-hidden">
       {isDarkMode ? (
       <div className="relative h-full w-full -z-10 bg-slate-950"><div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>
       ) : (
