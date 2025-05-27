3c13c953cabd63a0dbcd65cb80fd6c0187be262e
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-05-28 02:37:58 +0800

    d

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -7,17 +7,16 @@ const DarkMode = () => {
   return (
     <button
       onClick={() => {
-         console.log('Toggle clicked');
         setIsDarkMode(!isDarkMode)}}
       className="rounded text-gray-600 dark:text-gray-300 cursor-pointer 
       transition-transform duration-300 hover:scale-105 flex-shrink-0 
-      text-sm  sm:pt-3 z-10 pt-1.5"
+      sm:pt-3 z-10 pt-1.5 pr-3 sm:pr-0"
       aria-label="Toggle Dark Mode"
     >
       {isDarkMode ? (
-        <i className="fa-solid fa-moon text-sm sm:text-lg"></i>
+        <i className="fa-solid fa-moon text-lg"></i>
       ) : (
-        <i className="fa-solid fa-sun text-sm sm:text-lg"></i>
+        <i className="fa-solid fa-sun text-lg"></i>
       )}
     </button>
   );
