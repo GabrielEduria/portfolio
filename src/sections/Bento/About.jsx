768bca3ddb8fae97d750e783d806bb6c23f923c0
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-05-31 02:31:23 +0800

    s

--- src/sections/Bento/About.jsx
+++ src/sections/Bento/About.jsx
@@ -5,17 +5,16 @@ const About = () => {
           About
         </h2>
     <p className="mb-3 leading-relaxed sm:leading-7 overflow-hidden text-base">
-    A CS student eager to gain hands-on experience as a Front-End Developer. 
-    I specialize in building clean, responsive, and user-focused web applications 
-    using Next.js and React. My goal is to create seamless digital experiences 
-    that not only look great but also help businesses grow and solve real-world 
-    problems.
+    A CS student eager to gain hands-on experience as a Junior Developer.
+    I use Next.js and React to build clean, responsive, and user-focused web applications.
+    I help businesses grow — from emerging startups to established service providers — by creating
+    seamless digital experiences that not only look great but also solve real-world problems.
     </p>
 
 
       <p className="mb-3 leading-relaxed sm:leading-7 overflow-hidden text-base">
       Outside of coding, I enjoy coffee, hitting the gym, and staying active with basketball and other things.  
-      If I’m not working out or keeping up with NBA, you’ll find me spending time with my dog, Chonsi.
+      If I’m not working out or keeping up with UFC, you’ll find me spending time with my dog, Chonsi.
       </p>
 
       </div>
