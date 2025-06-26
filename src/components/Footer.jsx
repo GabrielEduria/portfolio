975ed9be5b3e2da5ad1069cf071bacb24542b766
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-06-26 21:12:26 +0800

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <div className="flex items-center justify-center pt-10 pb-5">
-        <h3 className="">© 2025 Gabriel Eduria `(Gabbbyyyy)`. All Rights Reserved.</h3>
+        <h3 className="">© 2025 Gabriel Eduria. All Rights Reserved.</h3>
       
     </div>
   )
