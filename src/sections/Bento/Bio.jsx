93b15f0c3fcd8eff17a1e8df4be5290b2a08897a
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-07-22 01:12:42 +0800

    fixed bio

--- src/sections/Bento/Bio.jsx
+++ src/sections/Bento/Bio.jsx
@@ -1,49 +1,52 @@
+const bio = [
+  {
+    year: '2004',
+    description: 'Born in Manila, philippines'
+  },
+  {
+    year: '2018',
+    description: '("Hello World")'
+  },
+  {
+    year: '2019',
+    description: 'First day hitting the Gym'
+  },
+  {
+    year: '2020',
+    description: 'Studied ABM'
+  },
+  {
+    year: '2022',
+    description: 'Started studying Computer Science'
+  },
+  {
+    year: '2023',
+    description: 'Started taking projects'
+  },
+  {
+    year: '2026',
+    description: 'Expected Graduation 🎓'
+  },
+  {
+    year: '2026',
+    description: 'And Many More 🤣😉🤞🌹' 
+  }
+]
 const Bio = () => {
     return (
       <div className="p-3">
         <h2 className="text-2xl font-bold mb-3 ">Bio</h2>
-         
-             <div className="flex items-baseline gap-2 sm:gap-4 pb-4 leading-7">
-                <p className="text-lg font-bold">2004</p>
-                <p className="text-base">Born in Manila, Philippines</p>
-              </div>
-
-               <div className="flex items-baseline gap-2 sm:gap-4 pb-4 leading-7">
-                <p className="text-lg font-bold">2018</p>
-                <p className="text-base">("Hello World")</p>
-              </div>
-
-              <div className="flex items-baseline gap-2 sm:gap-4 pb-4 leading-7">
-                <p className="text-lg font-bold">2019</p>
-                <p className="text-base">First day hitting the Gym</p>
-              </div>
-
-              <div className="flex items-baseline gap-2 sm:gap-4 pb-4 leading-7">
-                <p className="text-lg font-bold">2020</p>
-                <p className="text-base">Studied ABM</p>
-              </div>
-
-              <div className="flex items-baseline gap-2 sm:gap-4 pb-4 leading-7">
-                <p className="text-lg font-bold">2022</p>
-                <p className="text-base">Started studying Computer Science</p>
-              </div>
-
-              <div className="flex items-baseline gap-2 sm:gap-4 pb-4 leading-7">
-                <p className="text-lg font-bold">2023</p>
-                <p className="text-base">Started taking projects</p>
-              </div>
-
-
-              <div className="flex items-baseline gap-2 sm:gap-4 pb-4 leading-7">
-                <p className="text-lg font-bold">2026</p>
-                <p className="text-base">Expected Graduation 🎓</p>
-              </div>
-
-               <div className="flex items-baseline gap-2 sm:gap-4 pb-4 leading-7">
-                <p className="text-lg font-bold">2026</p>
-                <p className="text-base">And Many More 🤣😉🤞🌹</p>
-              </div>
-        
+              {bio.map((info, index) => {
+                return( 
+                  <div 
+                    key={index} 
+                    className="flex items-baseline gap-2 sm:gap-4 pb-4 leading-7"
+                  >
+                    <p className="text-lg font-bold">{info.year}</p>
+                    <p className="text-base">{info.description}</p>
+                  </div>
+                )
+              })}
       </div>
 
     )
