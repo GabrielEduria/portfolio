b10b486e5388b9597c33c80b569161c1d2134d5e
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-08-15 21:32:39 +0800

    .spacing

--- src/sections/Bento/BentoGrid.jsx
+++ src/sections/Bento/BentoGrid.jsx
@@ -10,7 +10,6 @@ import Designs from "./Designs";
 const BentoGrid = () => {
   return (
     <div className="w-full gap-2 grid grid-cols-1 md:grid-cols-12 auto-rows-auto rounded-lg mt-3 relative">
-      
       <div
         className="border border-gray-300 col-span-12 md:col-span-8 md:row-span-2 rounded-xl order-1 bg-cover bg-center"
       >
@@ -40,13 +39,11 @@ const BentoGrid = () => {
       >
         <TechStack />
       </div>
-
       <div
         className="border border-gray-300 col-span-12 md:col-span-8 md:row-span-4 rounded-lg order-4 md:order-5 bg-cover bg-center"
       >
         <Designs />
       </div>
-
       <div
         className="border border-gray-300 col-span-12 md:col-span-4 md:row-span-4 rounded-lg order-4 md:order-5 bg-cover bg-center"
       >
