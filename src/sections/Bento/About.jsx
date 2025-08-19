f6d6ded915af13612a608589dbae41f3b694c11c
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-08-19 21:22:03 +0800

    yo

--- src/sections/Bento/About.jsx
+++ src/sections/Bento/About.jsx
@@ -4,16 +4,16 @@ const About = () => {
         <h2 className="text-2xl font-bold pb-1 sm:pb-3">
           About
         </h2>
-    <p className="pb-3 leading-relaxed sm:leading-7 overflow-hidden text-base">
-     A CS student based in Asia, I use React to build clean web app
-     for brands, startups, and businesses. I like to improve and learn something new 
-     every day, and I find real satisfaction in building and designing things I enjoy.
-    </p>
+        <p className="pb-3 leading-relaxed sm:leading-7 overflow-hidden text-base">
+          A CS student based in Asia, I use React to build clean web app
+          for brands, startups, and businesses. I like to improve and learn something new 
+          every day, and I find real satisfaction in building and designing things I enjoy.
+        </p>
 
-    <p className="b-3 leading-relaxed sm:leading-7 overflow-hidden text-base">
-    Outside of coding, I enjoy brewing coffee, hitting the gym, and staying active with basketball and other things.  
-    If I'm not working out or keeping up with UFC, you'll find me spending time with my dog, Chonsi.
-    </p>
+        <p className="b-3 leading-relaxed sm:leading-7 overflow-hidden text-base">
+          When I'm not coding, I enjoy brewing coffee, hitting the gym, and staying active with basketball and other things.  
+          If I'm not working out or keeping up with UFC, you'll find me spending time with my dog, Chonsi.
+        </p>
 
       </div>
     )
