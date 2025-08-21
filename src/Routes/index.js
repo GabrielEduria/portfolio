befe4a80bda167ba34995a75ed1ab4ca61b1cb3f
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-08-21 19:59:04 +0800

    .

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,6 +3,7 @@ import Footer from "../components/Footer"
 import Projects from "./Projects";
 import Home from "./Home";
 import ResumePage from "./ResumePage";
+
 export {
     Header,
     Footer,
